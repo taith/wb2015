b3b60a8681005ee45b50151ead9a06d1416bd3ae
taith <taisvbk@gmail.com>
2015-01-31 11:34:55 +0000

    fix interface

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -1,6 +1,7 @@
 @extends('master')
 
 @section('content')
+<h3>Home</h3>
 <div class="clear-fix"></div> <!-- fix position -->
 <div class="home-content">
   <h2>Welcome to the Higher Education Finance Analytics (HEFA) System </h2>
@@ -14,7 +15,6 @@
   <div class="row">
     <label for="chart-2" class="col-sm-12 control-label">Example Chart 2</label>
     <div class="chart-2 col-md-12"></div>
-    <div>
   </div>
   </div>
 </div>
